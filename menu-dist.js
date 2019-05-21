5adf36fa7b097bb3ce99fd1e2f2c4d5cd8d9d4d8
Dorian <aherasm@externos.bankia.com>
2019-05-21 10:48:23 +0200

    responsive menu version 1

---
+++ menu-dist.js
@@ -0,0 +1,1 @@
+"use strict";var iconMenu=document.getElementById("icon-menu"),mainMenu=document.getElementById("main-menu");iconMenu.addEventListener("click",function(){mainMenu.classList.toggle("menu--show")});
---
+++ menu.js
@@ -0,0 +1,9 @@
+const iconMenu = document.getElementById('icon-menu');
+const mainMenu = document.getElementById('main-menu');
+
+iconMenu.addEventListener('click', ()=>mainMenu.classList.toggle('menu--show'));
+
+/*
+iconMenu.addEventListener('click', function () {
+    mainMenu.classList.toggle('menu--show');
+})*/
